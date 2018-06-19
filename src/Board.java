f6ab1866718597acb266d2eb4994ba824e419929
Brett <brp5088@rit.edu>
2018-06-19 19:46:50 -0400

    Started Working on my words with friends Helper. just getting the start up files made.

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Brett Patterson on 6/19/2018.
+ */
+public class Board {
+
+
+    public Board(){
+
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Brett Patterson on 6/19/2018.
+ */
+public class Node {
+
+    private int row;
+    private int col;
+
+    public Node(int r, int c){
+        row = r;
+        col = c;
+    }
+}
